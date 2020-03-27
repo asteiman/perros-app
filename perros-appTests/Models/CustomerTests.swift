287aa561423d21173ddfafd2a7ac204d8a97e43d
Alan Steiman <asteiman@gmail.com>
2020-03-27 15:01:07 +0800

    Add unit tests for models

---
+++ perros-appTests/Models/CustomerTests.swift
@@ -0,0 +1,18 @@
+//
+//  CustomerTests.swift
+//  perros-appTests
+//
+//  Created by Alan Steiman on 27/03/2020.
+//  Copyright © 2020 Alan Steiman. All rights reserved.
+//
+
+import XCTest
+@testable import perros_app
+
+class CustomerTests: XCTestCase {
+    
+    func test_statusLabel() {
+        let customer = Customer(id: 1, name: "name", address: "name")
+        XCTAssertEqual("https://picsum.photos/id/1/100", customer.avatar)
+    }
+}
---
+++ perros-appTests/Models/OrdersDashboardTests.swift
@@ -0,0 +1,31 @@
+//
+//  OrdersDashboardTests.swift
+//  perros-appTests
+//
+//  Created by Alan Steiman on 27/03/2020.
+//  Copyright © 2020 Alan Steiman. All rights reserved.
+//
+
+import XCTest
+@testable import perros_app
+
+class OrdersDashboardTests: XCTestCase {
+    
+    func test_equal() {
+        let dash = DashboardResponse(customers: [], orders: [], billing: [])
+        let dash2 = DashboardResponse(customers: [], orders: [], billing: [])
+        XCTAssertEqual(dash, dash2)
+    }
+    
+    func test_equal2() {
+        let dash = DashboardResponse(customers: [], orders: [], billing: [BillingDashboard(year: "2020", total: 10)])
+        let dash2 = DashboardResponse(customers: [], orders: [], billing: [BillingDashboard(year: "2020", total: 10)])
+        XCTAssertEqual(dash, dash2)
+    }
+    
+    func test_notequal() {
+        let dash = DashboardResponse(customers: [CustomerDashboard(id: 1, year: "", name: "", total: 1)], orders: [], billing: [])
+        let dash2 = DashboardResponse(customers: [], orders: [], billing: [])
+        XCTAssertNotEqual(dash, dash2)
+    }
+}
